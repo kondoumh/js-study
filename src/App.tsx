268a47e93f47c815d29ac7ce745e5b8b90f1c961
kondoumh <kondoumh@gmail.com>
2018-09-19 01:29:07 +0900

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
-import TodoList from './TodoList';
+
+interface TodoProps {
+  id: number;
+  title: string;
+  desc: string;
+  done: boolean;
+}
 
 class App extends React.Component {
-  constructor(props: any) {
+  constructor(props: TodoProps) {
     super(props);
     this.state = {
       todos: [
@@ -31,12 +37,6 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          Todo List
-        </p>
-        <TodoList
-          todo={this.state.todos}
-        />
       </div>
     );
   }
