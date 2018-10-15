b213b1223bfe1081c10b18cdb85c7628136d8cb8
kondoumh <kondoumh@gmail.com>
2018-10-15 11:15:38 +0900

    WIP add delete button

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -5,11 +5,19 @@ class TodoList extends React.Component<any, any> {
     public render() {
         return (
             <ul>
-                {this.props.items.map(item => (
-                    <li key={item.id}>{item.text}</li>
+                {this.props.items.map((item, i) => (
+                    <li key={item.id}>
+                    <input type="button" value="☓" onClick={this.handleClick} />{item.text}</li>
                 ))}
             </ul>
         )
+    }
+
+    private handleClick(i: any) {
+        this.state.todo.splice(i, 1);
+        this.setState({
+            todo : this.state.todo
+        });
     }
 }
 
