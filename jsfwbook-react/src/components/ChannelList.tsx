81a319e6bf94b799e991c5d16e8d875c12dae9ff
kondoumh <kondoumh@gmail.com>
2018-11-10 16:22:27 +0900

    Construct ChannelList and Replace with functional component

--- jsfwbook-react/src/components/ChannelList.tsx
+++ jsfwbook-react/src/components/ChannelList.tsx
@@ -1,9 +1,30 @@
-import * as React from 'react'
+import * as React from 'react';
+import { Menu, Icon} from 'semantic-ui-react';
+import { Link, NavLink } from 'react-router-dom'
 
-export class ChannelList extends React.Component<{}, {}> {
-    public render() {
-        return (
-            <div />
-        );
-    }
-}
+const channels = ['general', 'random'];
+
+export const ChannelList = () => {
+    return (
+        <Menu inverted vertical fixed={'left'}>
+            <Menu.Item as={Link} to={'/'}>
+                Home
+                <Icon name='home' />
+            </Menu.Item>
+            <Menu.Item>
+                Channels
+                <Menu.Menu>
+                    {channels.map(channel =>
+                        <Menu.Item
+                            key={channel}
+                            name={channel}
+                            as={NavLink}
+                            to={{ pathname: `/channels/${channel}`}}>
+                            {channel}
+                        </Menu.Item>
+                    )}
+                </Menu.Menu>
+            </Menu.Item>
+        </Menu>
+    );
+};
