0f15bdc6273c4bd899da01a34554dd2ae95674c5
kondoumh <kondoumh@gmail.com>
2018-11-16 06:50:56 +0900

    add MessageForm

---
+++ jsfwbook-react/src/components/MessageForm.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { postMessage, Message } from '../client';
+import { Button, Form, Segment, TextArea } from 'semantic-ui-react';
+
+interface MessageFormProps {
+    channelName: string;
+}
+
+interface MessageFormState {
+    body?: string
+}
+
+export class MessageForm extends React.Component<MessageFormProps, MessageFormState> {
+
+    constructor(props: MessageFormProps) {
+        super(props);
+        this.state = {
+            body: ''
+        };
+        this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    }
+
+    private handleTextAreaChange(event: React.FormEvent<HTMLTextAreaElement>) {
+        event.preventDefault();
+        this.setState({ body: event.currentTarget.value });
+    }
+
+    public render() {
+        return (
+            <Segment basic textAlign='center'>
+                <Form>
+                    <Form.Field>
+                        <TextArea
+                            autoHeight
+                            placeholder='Write your message'
+                            value={this.state.body}
+                            onChange={this.handleTextAreaChange} />
+                    </Form.Field>
+                    <Button primary type='submit'>Send</Button>
+                </Form>
+                <p>入力中の値: {this.state.body}</p>
+            </Segment>
+        );
+    }
+}
