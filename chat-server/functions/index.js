309e0a784e68561b9fb1c05c1895a5fd80ce48b4
kondoumh <kondoumh@gmail.com>
2018-11-24 11:10:02 +0900

    fix initialization fix instansiation

--- chat-server/functions/index.js
+++ chat-server/functions/index.js
@@ -11,7 +11,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const express = require('express');
-const app = express;
+const app = express();
 
 const cors = require('cors')({origin: true});
 app.use(cors);
