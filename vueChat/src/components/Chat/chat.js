3f2a23575fd67ff132a4894a137b1a0b7db2639c
kondoumh <kondoumh@gmail.com>
2018-11-29 02:27:14 +0900

    add channel messages binding

--- vueChat/src/components/Chat/chat.js
+++ vueChat/src/components/Chat/chat.js
@@ -1,9 +1,16 @@
 export default {
     name: 'chat',
+    methods: {
+        send_message() {
+            this.messages.push(this.message)
+            this.message = ""
+        }
+    },
     data() {
         return {
             channels: ["general", "random"],
-            message: ""
+            message: "",
+            messages: []
         }
     }
 }
