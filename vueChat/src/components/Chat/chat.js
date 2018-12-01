ccfa00ef916ef8c368594cf2bae47ebe97c286ea
kondoumh <kondoumh@gmail.com>
2018-12-01 14:17:03 +0900

    apply store

--- vueChat/src/components/Chat/chat.js
+++ vueChat/src/components/Chat/chat.js
@@ -1,16 +1,25 @@
+import { mapGetters, mapActions} from 'vuex'
+
 export default {
     name: 'chat',
+    computed: {
+     ...mapGetters([
+            'messages'
+        ]),
+    },
     methods: {
-        send_message() {
-            this.messages.push(this.message)
+     ...mapActions([
+        SET_MESSAGE
+     ]),
+     send_message() {
+            this.SET_MESSAGE(this.message) // this.messages.push(this.message)
             this.message = ""
         }
     },
     data() {
         return {
             channels: ["general", "random"],
-            message: "",
-            messages: []
+            message: ""
         }
     }
 }
