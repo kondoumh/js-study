919dca683bbc3c351df15fcbff11cf5b29242fbb
kondoumh <kondoumh@gmail.com>
2018-12-02 14:15:57 +0900

    cosmetic

--- vueChat/src/components/Chat/chat.js
+++ vueChat/src/components/Chat/chat.js
@@ -8,23 +8,22 @@ export default {
       this.GET_MESSAGES(this.$route.params.cname)
     },
     computed: {
-        ...mapGetters([
-          'messages',
-          'channels'
-        ]),
-      },
+      ...mapGetters([
+        'messages',
+        'channels'
+      ]),
+    },
     methods: {
-        ...mapActions([
-          GET_CHANNELS,
-          "GET_MESSAGES",
-          "POST_MESSAGES"
-        ]),
-        send_message(){
-          // this.SET_MESSAGE(this.message)
-          this.POST_MESSAGES({"cname": this.$route.params.cname, "message": this.message})
-          this.message = ""
-        }
-      },
+      ...mapActions([
+        GET_CHANNELS,
+        "GET_MESSAGES",
+        "POST_MESSAGES"
+      ]),
+      send_message(){
+        this.POST_MESSAGES({"cname": this.$route.params.cname, "message": this.message})
+        this.message = ""
+      }
+    },
     data() {
         return {
             message: ""
