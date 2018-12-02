699d2e714862770049784c16334cf4d9ba06dcf4
kondoumh <kondoumh@gmail.com>
2018-12-02 12:31:59 +0900

    apply async await

--- vueChat/src/store/actions.js
+++ vueChat/src/store/actions.js
@@ -1,5 +1,6 @@
 import {
-    SET_MESSAGE, GET_CHANNELS
+    SET_MESSAGE,
+    GET_CHANNELS
 } from './mutation-types'
 
 export default {
@@ -7,10 +8,16 @@ export default {
         commit(SET_MESSAGE, message)
     },
     [GET_CHANNELS] ({commit}) {
-        fetch('https://us-central1-demoapp-cc44a.cloudfunctions.net/v1').then((response)=>{
+        async function fetch_api() {
+            const response = await fetch('https://us-central1-demoapp-cc44a.cloudfunctions.net/v1/channels')
+            const json = await response.json()
+            commit(GET_CHANNELS, json.channels)
+        }
+        fetch_api()
+        /*fetch('https://us-central1-demoapp-cc44a.cloudfunctions.net/v1/channels').then((response)=>{
             return response.json()
         }).then((json)=>{
             commit(GET_CHANNELS, json.channels)
-        })
+        })*/
     }
 }
