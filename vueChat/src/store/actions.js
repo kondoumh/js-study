2f117aaf6a9d1a9df44dec65ffc8a8ef09f341bd
kondoumh <kondoumh@gmail.com>
2018-12-02 15:41:31 +0900

    fix url

--- vueChat/src/store/actions.js
+++ vueChat/src/store/actions.js
@@ -1,44 +1,44 @@
 import {
-    GET_CHANNELS,
-    SET_MESSAGES
+  GET_CHANNELS,
+  SET_MESSAGES
 } from './mutation-types'
-
-const get_message_path = cname => 'https://us-central1-demoapp-cc44a.cloudfunctions.net/v1/channels' + cname + '/messages'
-
-async function fetch_get_messages(cname) {
-    const response = await fetch(get_message_path(cname))
-    const json = await response.json()
-    return json.messages
+  
+const get_message_path = cname => 'https://us-central1-demoapp-cc44a.cloudfunctions.net/v1/channels/' + cname + '/messages'
+  
+async function fetch_get_messages (cname) {
+  const response = await fetch(get_message_path(cname))
+  const json = await response.json()
+  return json.messages
 }
-
+  
 export default {
-    [GET_CHANNELS] ({commit}) {
-        async function fetch_api() {
-            const response = await fetch('https://us-central1-demoapp-cc44a.cloudfunctions.net/v1/channels')
-            const json = await response.json()
-            commit(GET_CHANNELS, json.channels)
-        }
-        fetch_api()
-    },
-    async GET_MESSAGES ({commit}, cname) {
-        const messages = await fetch_get_messages(cname)
-        commit(SET_MESSAGES, messages)
-    },
-    async POST_MESSAGES ({commit}, {cname, message}) {
-        const response = await fetch(get_message_path(cname), {
-            methos: 'POST',
-            body: JSON.stringify({
-                'body': message
-            }),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        const json = await response.json()
-        if (json.result === 'ok') {
-            const messages = await fetch_get_messages(cname)
-            commit(SET_MESSAGES, messages)
-        }
+  [GET_CHANNELS] ({commit}) {
+     async function fetch_api(){
+     const response = await fetch('https://us-central1-demoapp-cc44a.cloudfunctions.net/v1/channels')
+     const json = await response.json()
+     commit(GET_CHANNELS, json.channels)
+   }
+   fetch_api()
+  },
+  async GET_MESSAGES ({commit}, cname) {
+    const messages = await fetch_get_messages(cname)
+    commit(SET_MESSAGES, messages)
+  },
+  async POST_MESSAGES ({commit}, {cname, message}) {
+    const response = await fetch(get_message_path(cname), {
+      method: 'POST',
+      body: JSON.stringify({
+        'body': message
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await response.json()
+    if (json.result === 'ok') {
+      const messages = await fetch_get_messages(cname)
+      commit(SET_MESSAGES, messages)
     }
+  }
 }
