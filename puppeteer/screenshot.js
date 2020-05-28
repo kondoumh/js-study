b9697e27fe9f0e66db1c9c47ddeb0c34f25a7040
kondoumh <kondoumh@gmail.com>
2020-05-28 12:36:55 +0900

    handle exception

--- puppeteer/screenshot.js
+++ puppeteer/screenshot.js
@@ -1,10 +1,16 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1280, height: 800 })
-  await page.goto('https://www.nytimes.com/')
-  await page.screenshot({ path: 'nytimes.png', fullPage: true })
-  await browser.close()
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1280, height: 800 })
+    await page.goto('https://www.nytimes.com/')
+    await page.screenshot({ path: 'nytimes.png', fullPage: true })
+    await browser.close()
+  } catch (e) {
+    console.error(e)
+  } finally {
+    console.log("finished.")
+  }
 })()
