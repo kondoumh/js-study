8a3b7cd64839628618e04d93704e3c11e90c66a8
kondoumh <kondoumh@gmail.com>
2020-11-14 11:56:29 +0900

    apply jq

--- octokit-test/index.js
+++ octokit-test/index.js
@@ -1,6 +1,10 @@
 const { Octokit} = require("@octokit/rest");
+const jq = require("node-jq");
 
-(async () => {
+//getContent();
+getSha()
+
+async function getContent() {
   const octokit = new Octokit();
   const content = await octokit.repos.getContent({
     owner: "kondoumh",
@@ -9,4 +13,15 @@ const { Octokit} = require("@octokit/rest");
   });
   const data = new Buffer.from(content.data.content, content.data.encoding).toString();
   console.log(data);
-})();
+}
+
+async function getSha() {
+  const octokit = new Octokit();
+  const content = await octokit.repos.getContent({
+    owner: "kondoumh",
+    repo: "sbe",
+    path: "src"
+  })
+  res = await jq.run('.data | map({ name: .name, sha: .sha })', content, {input: 'json'});
+  console.log(res);
+}
