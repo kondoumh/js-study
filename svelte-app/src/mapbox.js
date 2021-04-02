9baedf4539c2e662e81d5bbbcb1403c738294e11
kondoumh <kondoumh@gmail.com>
2021-04-02 12:59:14 +0900

    fix

--- svelte-app/src/mapbox.js
+++ svelte-app/src/mapbox.js
@@ -1,7 +1,7 @@
 import mapbox from 'mapbox-gl';
 
 // https://docs.mapbox.com/help/glossary/access-token/
-mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
+mapbox.accessToken = ''; // MAPBOX_ACCESS_TOKEN
 
 const key = {};
 
