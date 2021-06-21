17ab8c3aa39a80bee7d1d22ef6e525275087d4fb
kondoumh <kondoumh@gmail.com>
2021-06-21 21:31:50 +0900

    WIP add fitness client

---
+++ fit-api-client/fit.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const url = require('url');
+const opn = require('open');
+const destroyer = require('server-destroy');
+
+const { google } = require('googleapis');
+const fitness = google.fitness('v1');
+
+const keyPath = path.join(__dirname, 'oauth2.keys.json');
+let keys = { redirect_uris: [''] };
+if (fs.existsSync(keyPath)) {
+  keys = require(keyPath).web;
+}
+
+const oauth2Client = new google.auth.OAuth2(
+  keys.client_id,
+  keys.client_secret,
+  keys.redirect_uris[0]
+);
+
+google.options({ auth: oauth2Client });
+
+async function authenticate(scopes) {
+  return new Promise((resolve, reject) => {
+    const authorizeUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: scopes.join(' '),
+    });
+    const server = http
+      .createServer(async (req, res) => {
+        try {
+          if (req.url.indexOf('/oauth2callback') > -1) {
+            const qs = new url.URL(req.url, 'http://localhost:3000')
+              .searchParams;
+            res.end('Authentication successful! Please return to the console.');
+            server.destroy();
+            const { tokens } = await oauth2Client.getToken(qs.get('code'));
+            oauth2Client.credentials = tokens; // eslint-disable-line require-atomic-updates
+            resolve(oauth2Client);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      })
+      .listen(3000, () => {
+        // open the browser to the authorize url to start the workflow
+        opn(authorizeUrl, { wait: false }).then(cp => cp.unref());
+      });
+    destroyer(server);
+  });
+}
+
+async function runSample() {
+  // retrieve user profile
+  const res = await fitness.users.dataset.aggregate({
+    userId: '410630221267-t0b93p2sl7fo6jtqans7na0ndl91vbqn.apps.googleusercontent.com',
+    requestBody: {
+
+    }
+  });
+  console.log(res.data);
+}
+
+const scopes = [
+  'https://www.googleapis.com/auth/fitness.activity.read',
+  'https://www.googleapis.com/auth/fitness.activity.write',
+  'https://www.googleapis.com/auth/fitness.body.read',
+  'https://www.googleapis.com/auth/fitness.body.write',
+];
+authenticate(scopes)
+  .then(client => runSample(client))
+  .catch(console.error);
