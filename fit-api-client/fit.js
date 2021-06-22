25a113951e45ece11c2f6eacb3da82c8a349e98c
kondoumh <kondoumh@gmail.com>
2021-06-22 09:20:34 +0900

    get steps a day

--- fit-api-client/fit.js
+++ fit-api-client/fit.js
@@ -7,11 +7,11 @@ const url = require('url');
 const opn = require('open');
 const destroyer = require('server-destroy');
 
-const { google } = require('googleapis');
+const {google} = require('googleapis');
 const fitness = google.fitness('v1');
 
 const keyPath = path.join(__dirname, 'oauth2.keys.json');
-let keys = { redirect_uris: [''] };
+let keys = {redirect_uris: ['']};
 if (fs.existsSync(keyPath)) {
   keys = require(keyPath).web;
 }
@@ -22,7 +22,7 @@ const oauth2Client = new google.auth.OAuth2(
   keys.redirect_uris[0]
 );
 
-google.options({ auth: oauth2Client });
+google.options({auth: oauth2Client});
 
 async function authenticate(scopes) {
   return new Promise((resolve, reject) => {
@@ -38,7 +38,7 @@ async function authenticate(scopes) {
               .searchParams;
             res.end('Authentication successful! Please return to the console.');
             server.destroy();
-            const { tokens } = await oauth2Client.getToken(qs.get('code'));
+            const {tokens} = await oauth2Client.getToken(qs.get('code'));
             oauth2Client.credentials = tokens; // eslint-disable-line require-atomic-updates
             resolve(oauth2Client);
           }
@@ -48,7 +48,7 @@ async function authenticate(scopes) {
       })
       .listen(3000, () => {
         // open the browser to the authorize url to start the workflow
-        opn(authorizeUrl, { wait: false }).then(cp => cp.unref());
+        opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
       });
     destroyer(server);
   });
@@ -57,17 +57,51 @@ async function authenticate(scopes) {
 async function runSample() {
   // retrieve user profile
   const res = await fitness.users.dataset.aggregate({
-    userId: '410630221267-t0b93p2sl7fo6jtqans7na0ndl91vbqn.apps.googleusercontent.com',
-    requestBody: {
+    // Aggregate data for the person identified. Use me to indicate the authenticated user. Only me is supported at this time.
+    userId: 'me',
 
-    }
+    // Request body metadata
+    requestBody: {
+      "aggregateBy": [
+        {
+          "dataTypeName": "com.google.step_count.delta",
+          "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+        },
+        {
+          "dataTypeName": "com.google.weight.summary",
+          "dataSourceId": "derived:com.google.weight:com.google.android.gms:merge_weight"
+        },
+      ],
+      //   "bucketByActivitySegment": {},
+      //   "bucketByActivityType": {},
+      //   "bucketBySession": {},
+      "bucketByTime": { "durationMillis": 86400000 },
+      "startTimeMillis": 1593640800000,
+      "endTimeMillis": 1593727199999,
+      //   "filteredDataQualityStandard": [],
+    },
   });
-  console.log(res.data);
+  const point = res.data.bucket[0].dataset[0].point[0];
+  console.log(point.originDataSourceId);
+  console.log(formatDate(point.startTimeNanos));
+  console.log(formatDate(point.endTimeNanos));
+  console.log(point.dataTypeName);
+  console.log(point.value[0].intVal);
+}
+
+function formatDate(timestamp) {
+  let date = new Date()
+  date.setTime(timestamp / 1000000)
+  const params = {
+    year: 'numeric', month: 'numeric', day: 'numeric',
+    hour: 'numeric', minute: 'numeric', second: 'numeric',
+    hour12: false
+  }
+  return date.toLocaleString("ja", params)
 }
 
 const scopes = [
   'https://www.googleapis.com/auth/fitness.activity.read',
-  'https://www.googleapis.com/auth/fitness.activity.write',
   'https://www.googleapis.com/auth/fitness.body.read',
   'https://www.googleapis.com/auth/fitness.body.write',
 ];
