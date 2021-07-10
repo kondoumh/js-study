be71a1541284fb554639c45131a5248bd4588089
kondoumh <kondoumh@gmail.com>
2021-07-10 15:06:20 +0900

    Add example of parsing yaml

--- yaml-processor/index.js
+++ yaml-processor/index.js
@@ -3,5 +3,39 @@ const path = require('path');
 const fs = require('fs');
 
 const yamlText = fs.readFileSync(path.join(__dirname, 'hello.yml'));
-const data = yaml.load(yamlText);
-console.log(data);
+const dsl = yaml.load(yamlText);
+console.log(dsl);
+
+let variables = {}
+
+function setValue(variable, value) {
+  variables[variable] = value;
+}
+
+const funcDic = {
+  print: console.log,
+  set: setValue
+};
+
+function evalArgs(args) {
+  let result = [];
+  args.forEach(arg => {
+    if (arg in variables) {
+      result.push(variables[arg]);
+    } else {
+      result.push(arg);
+    }
+  });
+  return result;
+}
+
+dsl.forEach(inst => {
+  if ('func' in inst && 'args' in inst) {
+    const funcName = inst['func'];
+    if (funcName in funcDic) {
+      const func = funcDic[funcName];
+      const args = evalArgs(inst['args']);
+      func.apply(this, args);
+    }
+  }
+});
