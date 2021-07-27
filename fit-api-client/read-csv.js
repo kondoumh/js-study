e388824975588389c74f16bebe6f1d3f44ad774f
kondoumh <kondoumh@gmail.com>
2021-07-27 09:14:09 +0900

    Add csv parse module

---
+++ fit-api-client/read-csv.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const parse = require("csv-parse/lib/sync");
+
+function getNano(day) {
+  const dt = new Date(day);
+  return dt.getTime() * 1000000;
+}
+
+const csv = fs.readFileSync("weight.csv");
+const records = parse(csv, { columns: true });
+
+const weights = records.map(v => {
+   return {
+     min: getNano(v.date + "T00:00:00"),
+     max: getNano(v.date + "T23:59:59"),
+     time: getNano(v.date + "T07:00:00"),
+     weight: v.weight
+   }
+  });
+
+console.log(weights);
