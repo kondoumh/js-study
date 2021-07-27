09f3df03f1366871a859329e7ef1a156b4fc4bc3
kondoumh <kondoumh@gmail.com>
2021-07-28 07:31:40 +0900

    Add eventhubs client project

---
+++ eventhubs/send.js
@@ -0,0 +1,20 @@
+const { EventHubProducerClient } = require("@azure/event-hubs");
+
+const connectionString = "EVENT HUBS NAMESPACE CONNECTION STRING";
+const eventHubName = "mheventhub";
+
+async function main() {
+  const producer = new EventHubProducerClient(connectionString, eventHubName);
+  const batch = await producer.createBatch();
+  batch.tryAdd({ body: "First event" });
+  batch.tryAdd({ body: "Second event" });
+  batch.tryAdd({ body: "Third event" });
+
+  await producer.sendBatch(batch);
+  await producer.close();
+  console.log("A batch of three events have been sent to the event hub");
+}
+
+main().catch( err => {
+  console.log("Error occurred: ", err);
+})
