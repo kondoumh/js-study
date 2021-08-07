76674123187de6c8978f3e47e271148dd1ff3ba5
kondoumh <kondoumh@gmail.com>
2021-08-07 18:52:10 +0900

    apply auth module

--- fit-api-client/put-weight.js
+++ fit-api-client/put-weight.js
@@ -1,59 +1,9 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const url = require('url');
-const opn = require('open');
-const destroyer = require('server-destroy');
-
 const { google } = require('googleapis');
 const fitness = google.fitness('v1');
 const { readCsv } = require('./read-csv');
-
-const keyPath = path.join(__dirname, 'oauth2.keys.json');
-let keys = { redirect_uris: [''] };
-if (fs.existsSync(keyPath)) {
-  keys = require(keyPath).web;
-}
-
-const oauth2Client = new google.auth.OAuth2(
-  keys.client_id,
-  keys.client_secret,
-  keys.redirect_uris[0]
-);
-
-google.options({ auth: oauth2Client });
-
-async function authenticate(scopes) {
-  return new Promise((resolve, reject) => {
-    const authorizeUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: scopes.join(' '),
-    });
-    const server = http
-      .createServer(async (req, res) => {
-        try {
-          if (req.url.indexOf('/oauth2callback') > -1) {
-            const qs = new url.URL(req.url, 'http://localhost:3000')
-              .searchParams;
-            res.end('Authentication successful! Please return to the console.');
-            server.destroy();
-            const { tokens } = await oauth2Client.getToken(qs.get('code'));
-            oauth2Client.credentials = tokens; // eslint-disable-line require-atomic-updates
-            resolve(oauth2Client);
-          }
-        } catch (e) {
-          reject(e);
-        }
-      })
-      .listen(3000, () => {
-        // open the browser to the authorize url to start the workflow
-        opn(authorizeUrl, { wait: false }).then(cp => cp.unref());
-      });
-    destroyer(server);
-  });
-}
+const { authenticate } = require('./auth');
 
 async function createDataSource() {
   const res = await fitness.users.dataSources.create({
