7e2d52ab4583bd5e5eb64bbe7c2f0abb5532beb0
kondoumh <kondoumh@gmail.com>
2021-09-15 19:31:02 +0900

    create collection

--- eleventy-from-scratch/.eleventy.js
+++ eleventy-from-scratch/.eleventy.js
@@ -1,5 +1,15 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
 module.exports = config => {
   config.addPassthroughCopy('./src/images');
+  config.addCollection('work', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+  });
+  config.addCollection('featuredWork', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md').filter(
+      x => x.data.featured)
+    );
+  });
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
