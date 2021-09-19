a9593f1a432769b6647caaeb04a35617582a4d16
kondoumh <kondoumh@gmail.com>
2021-09-19 10:38:53 +0900

    add filters

--- eleventy-from-scratch/.eleventy.js
+++ eleventy-from-scratch/.eleventy.js
@@ -1,6 +1,12 @@
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
 
 module.exports = config => {
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
   config.addPassthroughCopy('./src/images');
   config.addCollection('work', collection => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
