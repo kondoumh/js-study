356eda7c43474a64f4191b15924db96ea6afe5b6
kondoumh <kondoumh@gmail.com>
2021-09-23 15:17:18 +0900

    Add sample to call functions as string

---
+++ exec-func/funs.js
@@ -0,0 +1,11 @@
+module.exports = {
+  foo() {
+    console.log('foo');
+  },
+  bar() {
+    console.log('bar');
+  },
+  baz() {
+    console.log('baz');
+  }
+};
---
+++ exec-func/index.js
@@ -0,0 +1,14 @@
+const funs = require('./funs');
+
+try {
+  funs['foo']();
+  funs['bar']();
+  funs['baz']();
+  funs['hoge'](); // TypeError: funs.hoge is not a function
+} catch (e) {
+  if (e instanceof TypeError) {
+    console.error('function does not exists.', e.message);
+  } else {
+    console.error(e);
+  }
+}
