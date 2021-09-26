fd60cf6505e2df7eaf83223582ca2f021437638b
kondoumh <kondoumh@gmail.com>
2021-09-26 12:00:26 +0900

    add node-fetch example

---
+++ fetch/index.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch").default;
+
+(async() => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/todos/1',
+    {
+      headers: {
+        cookie: 'awsome cookie'
+      }
+    }
+  );
+  const data = await res.json();
+  console.log(data);
+})();
