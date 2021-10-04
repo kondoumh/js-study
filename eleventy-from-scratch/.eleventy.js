00ee0b7a84c96a2ea97378f83ed8c142dbbefb25
kondoumh <kondoumh@gmail.com>
2021-10-05 08:42:50 +0900

    add rss feed

--- eleventy-from-scratch/.eleventy.js
+++ eleventy-from-scratch/.eleventy.js
@@ -2,6 +2,7 @@ const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
 
 module.exports = config => {
   // Add filters
@@ -11,6 +12,8 @@ module.exports = config => {
   config.addCollection('work', collection => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
   });
+  // Plugins
+  config.addPlugin(rssPlugin);
   config.addCollection('featuredWork', collection => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md').filter(
       x => x.data.featured)
