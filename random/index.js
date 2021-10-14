c7d0e474f42851b3956ddafe4ac5955a8db5fb4a
kondoumh <kondoumh@gmail.com>
2021-10-15 08:32:55 +0900

    add function

--- random/index.js
+++ random/index.js
@@ -1,13 +1,20 @@
-const arr = [0, 1, 1];
+randomChoice(10);
+randomChoice(100);
+randomChoice(1000);
+randomChoice(10000);
+randomChoice(100000);
 
-let zero = 0;
-let one = 0
-for (i = 0; i < 100000; i++) {
-  const choice = arr[Math.floor(Math.random() * arr.length)];
-  if (choice === 0) {
-    zero++;
-  } else {
-    one++;
+function randomChoice(trial) {
+  const arr = [0, 1, 1];
+  let zero = 0;
+  let one = 0
+  for (i = 0; i < trial; i++) {
+    const choice = arr[Math.floor(Math.random() * arr.length)];
+    if (choice === 0) {
+      zero++;
+    } else {
+      one++;
+    }
   }
+  console.log(`traial ${trial} --- 0: ${zero}, 1: ${one}`);
 }
-console.log(`0: ${zero}, 1: ${one}`);
