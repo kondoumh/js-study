921aa346e88d611f7b1d694320f5f2215a129297
kondoumh <kondoumh@gmail.com>
2021-10-26 17:12:08 +0900

    add pg example

---
+++ node-postgres/index.js
@@ -0,0 +1,25 @@
+const { Client } = require('pg');
+
+var client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'hoge',
+  password: 'postgres',
+  port: 5432
+})
+
+client.connect();
+
+(async() => {
+  const query = {
+    text: 'select now();'
+  }
+  try {
+    const res = await client.query(query);
+    console.log(res.rows);
+  } catch (err) {
+    console.error(err.stack);
+  } finally {
+    client.end();
+  }
+})();
