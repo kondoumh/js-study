640bd77534dd57be6cd3d295963c393ac0f4af0e
kondoumh <kondoumh@gmail.com>
2023-04-08 14:44:49 +0900

    update

--- rss-feeder/index.js
+++ rss-feeder/index.js
@@ -5,11 +5,8 @@ let parser = new Parser();
 (async () => {
 
   let feed = await parser.parseURL('https://developer.mamezou-tech.com/feed');
-  console.log(feed.title);
-  console.log(feed.items.length);
 
   feed.items.forEach(item => {
-    const date = new Date(item.pubDate);
-    console.log(item.title + ':' + date.toLocaleDateString());
+    console.log('%s,%s,%s', item.title, item.pubDate, item.link);
   });
 })();
